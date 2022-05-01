1e2a48f72641e694cbf62fa09c14a9d8d5bf71ae
Hieu Le <hieu@littlelives.com>
2022-05-01 18:29:58 +0700

    core functions

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -3,6 +3,66 @@ defmodule ExBanking do
   Documentation for `ExBanking`.
   """
 
+  alias ExBanking.User
+
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user) when is_binary(user) do
+    User.start_link(user)
+    :ok
+  end
+
+  def create_user(_) do
+    {:error, :wrong_arguments}
+  end
+
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(_user, _amount, _currency) do
+    new_balance = 0
+    {:ok, new_balance}
+  end
+
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :user_does_not_exist
+             | :not_enough_money
+             | :too_many_requests_to_user}
+  def withdraw(_user, _amount, _currency) do
+    new_balance = 0
+    {:ok, new_balance}
+  end
+
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def get_balance(_user, _currency) do
+    balance = 0
+    {:ok, balance}
+  end
+
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) ::
+          {:ok, from_user_balance :: number, to_user_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :not_enough_money
+             | :sender_does_not_exist
+             | :receiver_does_not_exist
+             | :too_many_requests_to_sender
+             | :too_many_requests_to_receiver}
+  def send(_from_user, _to_user, _amount, _currency) do
+    from_user_balance = 0
+    to_user_balance = 0
+    {:ok, from_user_balance, to_user_balance}
+  end
+
   @doc """
   Hello world.
 
